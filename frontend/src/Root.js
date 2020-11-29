0d08c463bc358cceaf6a9fe5ae7026234c271d70
haruyasu <harukun2002@gmail.com>
2020-11-29 21:29:04 +0900

    add auth in react

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -6,8 +6,10 @@ import { applyMiddleware, createStore } from "redux";
 import { routerMiddleware, ConnectedRouter } from "connected-react-router";
 
 import rootReducer from "./Reducer";
+import { setCurrentUser, setToken } from "./components/login/LoginActions"; // new imports
+import { isEmpty } from "./utils/Utils"; // new imports
 
-const Root = ({ children, initialState = {} }) => {
+export default ({ children, initialState = {} }) => {
     const history = createBrowserHistory();
     const middleware = [thunk, routerMiddleware(history)];
 
@@ -17,11 +19,18 @@ const Root = ({ children, initialState = {} }) => {
         applyMiddleware(...middleware)
     );
 
+    // check localStorage
+    if (!isEmpty(localStorage.getItem("token"))) {
+        store.dispatch(setToken(localStorage.getItem("token")));
+    }
+    if (!isEmpty(localStorage.getItem("user"))) {
+        const user = JSON.parse(localStorage.getItem("user"));
+        store.dispatch(setCurrentUser(user, ""));
+    }
+
     return (
         <Provider store={store}>
             <ConnectedRouter history={history}>{children}</ConnectedRouter>
         </Provider>
     );
 };
-
-export default Root;
--- server/server/settings.py
+++ server/server/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
-    'apps.accounts'
+    'apps.accounts',
+    'corsheaders',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
 
 #configure DRF
@@ -61,6 +67,7 @@ DJOSER = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
