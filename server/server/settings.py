d12f0b8a4b1ecad99e1d8d1e27ba0b9e3a4bcfd3
haruyasu <harukun2002@gmail.com>
2020-11-30 13:06:19 +0900

    add CRUD in django and react

--- server/server/settings.py
+++ server/server/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
-    'apps.accounts',
     'corsheaders',
+    'apps.accounts',
+    'apps.notes',
 ]
 
 CORS_ALLOWED_ORIGINS = [
